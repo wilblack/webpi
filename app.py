865a557beb492f5812604c5f5ce16467ecf2d365
wilblack <wilblack21@gmail.com>
2013-06-28 21:55:43 -0700

    Initial commit.

---
+++ app.py
@@ -0,0 +1,49 @@
+from tornado import websocket, web, ioloop
+import tornado
+import json
+
+cl = []
+
+class IndexHandler(tornado.web.RequestHandler):
+
+    def get(self):
+        self.render("index.html")
+
+class SocketHandler(tornado.websocket.WebSocketHandler):
+
+    def open(self):
+        if self not in cl:
+            cl.append(self)
+
+    def on_close(self):
+        if self in cl:
+            cl.remove(self)
+
+class ApiHandler(tornado.web.RequestHandler):
+
+    @tornado.web.asynchronous
+    def get(self, *args):
+        self.finish()
+        id = self.get_argument("id")
+        value = self.get_argument("value")
+        timestamp = self.get_argument("timestamp")
+        data = {"id": id, "value" : value, "timestamp":timestamp}
+        data = json.dumps(data)
+        for c in cl:
+            c.write_message(data)
+
+    @tornado.web.asynchronous
+    def post(self):
+        pass
+
+app = tornado.web.Application([
+    (r'/', IndexHandler),
+    (r'/ws', SocketHandler),
+    (r'/api', ApiHandler),
+    (r'/(favicon.ico)', tornado.web.StaticFileHandler, {'path': '../'}),
+    (r'/(rest_api_example.png)', tornado.web.StaticFileHandler, {'path': './'}),
+])
+
+if __name__ == '__main__':
+    app.listen(8888)
+    tornado.ioloop.IOLoop.instance().start()
